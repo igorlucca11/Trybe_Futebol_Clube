2f4c46fe0d21fb836ec9061af70c5f4b4e0cfb27
Igor Lucca <=>
2023-05-16 22:37:38 -0300

    errors cases of the endpoint /teams

--- app/backend/src/errors.ts
+++ app/backend/src/errors.ts
@@ -1,14 +1,13 @@
-export default interface Error {
-  status: number,
-  message: string,
+class MyErrors extends Error {
+  public status: number;
+  constructor(message: string, status: number) {
+    super();
+    this.name = message;
+    this.message = message;
+    this.status = status;
+  }
 }
 
-export const ALL_FIELDS_MUST_BE_FILLED: Error = {
-  status: 400,
-  message: 'All fields must be filled',
-};
+export const ALL_FIELDS_MUST_BE_FILLED = new MyErrors('All fields must be filled', 400);
 
-export const INVALID_EMAIL_OR_PASSWORD: Error = {
-  status: 400,
-  message: 'Email or password incorrect',
-};
+export const INVALID_EMAIL_OR_PASSWORD = new MyErrors('Invalid email or password', 401);
--- app/backend/src/services/Users.ts
+++ app/backend/src/services/Users.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
+import testEmail from '../middleweres/emailTest';
 import createToken from '../middleweres/token';
-import { ALL_FIELDS_MUST_BE_FILLED } from '../errors';
+import { ALL_FIELDS_MUST_BE_FILLED, INVALID_EMAIL_OR_PASSWORD } from '../errors';
 import passwordValidation from '../middleweres/passwordValidation';
 import User from '../database/models/UserModel';
 
@@ -13,21 +14,17 @@ class servicesUsers {
   static async validateUser(req: Request, _res: Response, next: NextFunction) {
     const { email, password } = req.body;
     try {
-      if (!email || !password) {
-        throw new Error();
-      }
+      if (!email || !password) { throw ALL_FIELDS_MUST_BE_FILLED; }
       const user = await servicesUsers.getUser(email);
-      if (user === null) {
-        throw new Error();
-      }
-      const isPassValid = passwordValidation(password, user.password);
-      if (!isPassValid) {
-        throw new Error();
-      }
+      const isEmailValid = testEmail(email);
+      if (!isEmailValid || password.length < 6) { throw INVALID_EMAIL_OR_PASSWORD; }
+      if (user === null) { throw INVALID_EMAIL_OR_PASSWORD; }
+      const isPassValid = await passwordValidation(password, user.password);
+      if (!isPassValid) { throw INVALID_EMAIL_OR_PASSWORD; }
       req.body.username = user.username;
       next();
     } catch (err) {
-      next(ALL_FIELDS_MUST_BE_FILLED);
+      next(err);
     }
   }
 
